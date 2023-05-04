e718c1612bdea0f9a8c6dd94916834c19656f964
Abhishek Raut <87932393+Abhishek10-r@users.noreply.github.com>
2023-05-04 12:14:44 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Successfully made changes";
+		return "Successfully made changes ";
 	}
 
 //	echo "# github-actions-example" >> README.md
